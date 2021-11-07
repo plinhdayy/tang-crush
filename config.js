18c27f06ecaec6735a037147b9992d44470dd60c
plinhdayy <93864710+plinhdayy@users.noreply.github.com>
2021-11-07 21:34:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'kly à!',
+    introDesc: `chúc kly sớm hết f2 nhé học hành nhớ giữ sức khoẻ =))`,
+    btnIntro: 'ôk',
+    title: 'hihihihihihi',
+    desc: 'kậu iu tớ mà đúng kô ',
+    btnYes: 'đúng oyyy',
+    btnNo: 'Không',
+    question:'Trên thế giới hơn 7 tỉ người mà sao kly lại yêu plinh <3',
+    btnReply: 'Gửi cho plinh <3',
+    reply: 'tại vì plinh xinh géi học giỏi đáng iu đoóooo',
+    mess: 'ekm biết mà 🥰. iu kly nhiều nhiều 😘😘',
+    messDesc: 'bank plinh 100k nhé',
+    btnAccept: 'Okiiiii hoi',
+    messLink: 'https://m.me/nguyenphuonglinh0303' 
 }
